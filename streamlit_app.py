ecc39f21c993582d297ec90e9e01781d1c0204c9
rsanchezmartintraining <147501466+rsanchezmartintraining@users.noreply.github.com>
2023-10-14 11:42:41 +0200

    Fix title in button fruit list

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,7 @@ except URLError as e:
 # streamlit.header("The fruit load list contains:")
 # streamlit.dataframe(my_data_rows)
 
-streamlit.header("The fruit load list contains:")
+streamlit.header("View Our Fruit List - Add Your Favorites:")
 # Snowflake-related functions
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
@@ -58,7 +58,7 @@ def get_fruit_load_list():
     return my_cur.fetchall()
 
 # Add a button to load the fruit
-if streamlit.button('View Our Fruit List - Add Your Favorites'):
+if streamlit.button('Get Fruit List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
   my_cnx.close()
