b102208704350c602d20fcfeaccfcc81ca591bf9
rsanchezmartintraining <147501466+rsanchezmartintraining@users.noreply.github.com>
2023-10-14 10:50:29 +0200

    Temporary stop Snowflake connection

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,6 +34,9 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 
+# Don't run anaything past here while we troubleshoot
+streamlit.stop()
+
 import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
